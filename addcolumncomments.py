3f20e283173e15f3f68b4939c381a8d818aefb2a
codeliuyang <30414412+codeliuyang@users.noreply.github.com>
2021-08-29 20:31:03 +0800

    user 'show create table ...' to add comment

--- addcolumncomments.py
+++ addcolumncomments.py
@@ -1,7 +1,5 @@
 """ 给表和字段添加注释
-
 暂时仅使用MySQL
-
 ALTER TABLE supplier_seller COMMENT '联营商';
 
 ALTER TABLE children
@@ -14,7 +12,7 @@ from django.apps import apps
 
 
 class Command(BaseCommand):
-    help = 'alter table columns to add comment for columns'
+    help = 'alter table columns to add comment with the verbose_name of model'
 
     def add_arguments(self, parser):
         # 增加数据库配置
@@ -35,7 +33,6 @@ class Command(BaseCommand):
         connection = connections[database]
         connection.prepare_database()
         cursor = connection.cursor()
-
         # 2. 找出所有自己定义的models
         app_label = options['app_label']
         if app_label is None:
@@ -43,62 +40,46 @@ class Command(BaseCommand):
             return None
         custom_models = apps.get_app_config(app_label).models
         for model_name in custom_models:
-
             modelobj = custom_models[model_name]
-
-            # 如果自己定义了table_name，那就以定义的为准，否则就是默认的Django模型Model的类名为 应用名+下划线+模型类名
+            # 2.1 如果自己定义了table_name，那就以定义的为准，否则就是默认的Django模型Model的类名为 应用名+下划线+模型类名
             table_name = modelobj._meta.db_table
             if not table_name:
                 table_name = app_label + '_' + model_name
-
-            # 遍历属性
+            # 2.2 从数据库中获取 ddl of create table ...
+            ddl_sql = "show create table " + table_name
+            cursor.execute(ddl_sql)
+            ddl_result = cursor.fetchall()
+            table_ddl = ddl_result[0][1]
+            ddl_texts = table_ddl.split("\n")
+            ddl_column_dict = {}
+            for ddl_text in ddl_texts:
+                ddl_text = ddl_text.strip()
+                # 找到属于字段的行
+                if ddl_text[0] == '`':
+                    next_index = ddl_text.index("`", 1, len(ddl_text))
+                    column_key = ddl_text[1: next_index]
+                    if column_key == 'id':
+                        continue
+                        # 移除comment
+                    comment_index = ddl_text.find("COMMENT")
+                    ddl_column_dict[column_key] = ddl_text[0:comment_index]
+            # 3. 遍历model的字段
             fields = modelobj._meta.fields
             for field in fields:
-                model_comment_sql = "-- model_comment_sql for " + table_name + "\n"
-                model_comment_sql += "ALTER TABLE " + table_name + "\n"
-
                 db_column = field.db_column
-                verbose_name = field.verbose_name
-
-                if not verbose_name:
-                    continue
-
                 if not db_column:
                     db_column = field.name
-                model_comment_sql += "MODIFY COLUMN " + db_column
-                field_type = str(type(field))
-                if "AutoField" in str(field_type):
+                verbose_name = field.verbose_name
+                if not verbose_name or verbose_name == db_column.replace("_", " "):
                     continue
-                model_comment_sql += " " + model_filed_type_to_sql_type(field_type, field.max_length)
-
-                if field.null:
-                    if field.default is None:
-                        model_comment_sql += " " + "DEFAULT NULL"
-                    elif "NOT_PROVIDED" in str(field.default):
-                        model_comment_sql += " " + "DEFAULT NULL"
-                    elif field.default is False:
-                        model_comment_sql += ""
-                    else:
-                        model_comment_sql += " " + "DEFAULT '" + str(field.default) + "'"
-                else:
-                    model_comment_sql += " " + "NOT NULL"
-
-                model_comment_sql += " " + "COMMENT '" + str(verbose_name) + "'"
-
+                model_comment_sql = "-- model_comment_sql for " + table_name + "." + db_column + "\n"
+                model_comment_sql += "ALTER TABLE " + table_name + "\n"
+                model_comment_sql += "MODIFY COLUMN "
+                field_type = str(type(field))
+                if "AutoField" in str(field_type) or "Foreign" in str(field_type):
+                    continue
+                original_ddl = ddl_column_dict.get(db_column)
+                model_comment_sql += original_ddl + " COMMENT '" + str(verbose_name) + "'"
                 print(model_comment_sql)
-
                 cursor.execute(model_comment_sql)
                 connection.commit()
-
-
-def model_filed_type_to_sql_type(field_type, max_length):
-    if "DateTimeField" in field_type:
-        return "datetime(6)"
-    if "IntegerField" in field_type:
-        return "int(11)"
-    if "CharField" in field_type:
-        return "varchar(" + str(max_length) + ")"
-    if "FloatField" in field_type:
-        return "double"
-    if "BooleanField" in field_type:
-        return "tinyint(1)"
