4ff5ad3e04948c76173285223ddf5fe4a21170f4
yangliu <codeliuyang@163.com>
2022-06-19 22:55:45 +0800

    fix file name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="addcomments",
-    version="0.0.1",
+    version="0.0.2",
     author="yangliu",
     author_email="codeliuyang@163.com",
     description="add database table column comments",
